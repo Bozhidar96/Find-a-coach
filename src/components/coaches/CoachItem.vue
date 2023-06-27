70dd4c4b4aa0cffa1eefa4c7c8f989cd98ebe55b
bozhidar.rusev <bojidar_rusev96@abv.bg>
2023-06-27 23:29:02 +0300

    Coaches to Vuex

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -3,11 +3,18 @@
     <h3>{{ fullname }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
     </div>
     <div class="actions">
-      <base-button mode="outline" link :to="coachContactLink">Contact</base-button >
-      <base-button link :to="coachDetailsLink">Details</base-button >
+      <base-button mode="outline" link :to="coachContactLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDetailsLink">Details</base-button>
     </div>
   </li>
 </template>
