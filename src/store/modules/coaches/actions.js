7cd0c462fab50d8e91957ca8229582ce6e0f074d
bozhidar.rusev <bojidar_rusev96@abv.bg>
2023-06-28 20:22:44 +0300

    sending coaching requests http

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -17,12 +17,36 @@ export default {
       }
     );
 
-    // const responseData = await response.json();
+    const responseData = await response.json();
 
     if (!response.ok) {
-      // error ...
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
 
     context.commit('registerCoach', { ...coachData, id: userId });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-c4f46-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //... impl error
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   },
 };
